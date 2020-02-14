03159ab4110e43d5b5858b725f202a2e04b8158d
momo <momo1124a@icloud.com>
2020-02-14 10:37:53 +0000

    Implement basic login

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,9 +2,9 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
 
-  def setup
+    def setup
     @user = User.new(name: "Example User", email: "user@example.com",
-                 password: "foobar", password_confirmation: "foobar")
+                     password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
@@ -42,12 +42,12 @@ class UserTest < ActiveSupport::TestCase
   
   test "email validation should reject invalid addresses" do
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
-                           foo@bar_baz.com foo@bar+baz.com foo@bar..com]
+                           foo@bar_baz.com foo@bar+baz.com]
     invalid_addresses.each do |invalid_address|
       @user.email = invalid_address
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
-   end
+  end
    
     test "email addresses should be unique" do
     duplicate_user = @user.dup
